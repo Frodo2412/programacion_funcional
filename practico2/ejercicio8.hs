3575ac1cea134c21e2c404aa14e2b6455dd006c2
Elizabeth Bennett <elibnt.07@gmail.com>
2023-04-10 21:32:05 -0300

    Ejercicio8

---
+++ practico2/ejercicio8.hs
@@ -0,0 +1,11 @@
+{-Usando map, defina una funcion squares :: [Int ] → [Int ] que dada una lista 
+de enteros retorne una lista con los cuadrados de los elementos de la lista-}
+
+
+squares :: [Int] -> [Int]
+squares = map (\x -> x * x)
+
+
+
+squaresGenerico :: Num a => [a] -> [a]
+squaresGenerico = map (\x -> x * x)
